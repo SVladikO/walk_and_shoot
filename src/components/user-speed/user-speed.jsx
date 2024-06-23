d9b8b4216709d3fcfe4649d86cdc7f3f2ecf0aac
svladiko <vlad.serhiychuk@gmail.com>
2024-06-23 11:30:26 +0300

    Added change speed.

--- src/components/user-speed/user-speed.jsx
+++ src/components/user-speed/user-speed.jsx
@@ -1,13 +1,22 @@
+import {useState} from 'react';
+
 import {Wrapper, InputWrapper} from './user-speed.style';
+import {game} from '../../util/glob';
 
 export default function UserSpeed() {
+    const [userSpeed, setUserSpeed] = useState(game.user?.step || 1);
+    const changeUserSpeed = i => {
+        game.user.step += i;
+        setUserSpeed(game.user.step)
+    }
+
     return (
         <Wrapper>
             user speed
             <InputWrapper>
-                <span>-</span>
-                <input type="number" value="1" onChange={() => {}}/>
-                <span>+</span>
+                <span onClick={() => changeUserSpeed(-1)}>-</span>
+                <input type="number" value={userSpeed} onChange={() => {}}/>
+                <span  onClick={() => changeUserSpeed(1)}>+</span>
             </InputWrapper>
         </Wrapper>
     )
